931f3d2252982c9ea6004f63b88054647580d9ce
nikoloz.mikeladze <nikolozm33@gmail.com>
2024-08-23 16:14:29 +0200

    commit

---
+++ exam.py
@@ -0,0 +1,48 @@
+#მისალმება
+print("mogesalmebit chven bankshi")
+
+#ბარატის მოთავსება ბანკომატში
+print("")
+print("gtxovt moatavset tqveni barati bankomatshi an miadet specialur wamkitxvels")
+
+#პინკოდი
+print("gtxovt miutitet tqveni pinkodi")
+print("")
+print(input("please enter pincode:"))
+print("")
+
+#ბალანსი
+print("tqven balanse gaqvt 10000₾")
+
+
+#თანხის შეტანა
+print("")
+shetana =int(input('sheitanet tanxa: '))
+num=10000
+print(num + shetana, "lari")
+print("")
+
+
+#გამოტანა
+gamotana =int(input('gamoitanet tanxa: '))
+print(num + shetana - gamotana, "lari")
+print("")
+
+
+
+
+#ბალანსის გაგება
+balance= num + shetana - gamotana
+print("gtxovt, miutitot sworad")
+balansi=(input("balansis gagebistvis sheitanet brdzaneba 'gageba' "))
+print(balance, "lari")
+
+
+#დასრულება
+print("")
+dasruleba="madloba rom sargeblobt chveni servisit, naxvamdis"
+print("")
+print("gtxovt, miutitot sworad")
+print (input("operaciis dasrulebistvis daweret 'dasruleba' "))
+print("")
+print(dasruleba)
